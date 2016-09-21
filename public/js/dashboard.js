b8a38415904fda0402b67830d56cfaed125fcef7
Matt <matthewsharp123@gmail.com>
2016-09-21 11:11:49 +0100

    basic dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,7 +4,9 @@ google.charts.load('current', {'packages':['corechart']});
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(pieChart);
 google.charts.setOnLoadCallback(lineChart);
-google.charts.setOnLoadCallback(columnChart);
+google.charts.setOnLoadCallback(columnChart1);
+google.charts.setOnLoadCallback(columnChart2);
+google.charts.setOnLoadCallback(columnChart3);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
@@ -23,8 +25,9 @@ function pieChart() {
   ]);
   // Set chart options
   var options = {'title':'How Much Pizza I Ate Last Night',
-    'width':400,
-    'height':300};
+    'height': '100%',
+    colors: ['#64b5f6', '#00bcd4', '#f44336', '#9c27b0', '#e91e63']
+  };
 
   // Instantiate and draw our chart, passing in some options.
   var chart = new google.visualization.PieChart(document.getElementById('chart1'));
@@ -51,13 +54,13 @@ function lineChart() {
   chart.draw(data, options);
 }
 
-function columnChart() {
+function columnChart1() {
   var data = google.visualization.arrayToDataTable([
     ["Element", "Density", { role: "style" } ],
-    ["Copper", 8.94, "#b87333"],
-    ["Silver", 10.49, "silver"],
-    ["Gold", 19.30, "gold"],
-    ["Platinum", 21.45, "color: #e5e4e2"]
+    ["Copper", 8.94, 'color: #64b5f6'],
+    ["Silver", 10.49, "color: #00bcd4"],
+    ["Gold", 19.30, "color: #f44336"],
+    ["Platinum", 21.45, "color: #9c27b0"]
   ]);
 
   var view = new google.visualization.DataView(data);
@@ -70,11 +73,64 @@ function columnChart() {
 
   var options = {
     title: "Density of Precious Metals, in g/cm^3",
-    width: 600,
-    height: 400,
+    width: '100%',
     bar: {groupWidth: "95%"},
     legend: { position: "none" },
   };
   var chart = new google.visualization.ColumnChart(document.getElementById("chart3"));
+  chart.draw(view, options);
+}
+
+function columnChart2() {
+  var data = google.visualization.arrayToDataTable([
+    ["Element", "Density", { role: "style" } ],
+    ["Copper", 8.94, 'color: #64b5f6'],
+    ["Silver", 10.49, "color: #00bcd4"],
+    ["Gold", 19.30, "color: #f44336"],
+    ["Platinum", 21.45, "color: #9c27b0"]
+  ]);
+
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0, 1,
+    { calc: "stringify",
+      sourceColumn: 1,
+      type: "string",
+      role: "annotation" },
+  2]);
+
+  var options = {
+    title: "Density of Precious Metals, in g/cm^3",
+    width: '100%',
+    bar: {groupWidth: "95%"},
+    legend: { position: "none" },
+  };
+  var chart = new google.visualization.ColumnChart(document.getElementById("chart4"));
+  chart.draw(view, options);
+}
+
+function columnChart3() {
+  var data = google.visualization.arrayToDataTable([
+    ["Element", "Density", { role: "style" } ],
+    ["Copper", 8.94, 'color: #64b5f6'],
+    ["Silver", 10.49, "color: #00bcd4"],
+    ["Gold", 19.30, "color: #f44336"],
+    ["Platinum", 21.45, "color: #9c27b0"]
+  ]);
+
+  var view = new google.visualization.DataView(data);
+  view.setColumns([0, 1,
+    { calc: "stringify",
+      sourceColumn: 1,
+      type: "string",
+      role: "annotation" },
+  2]);
+
+  var options = {
+    title: "Density of Precious Metals, in g/cm^3",
+    width: '100%',
+    bar: {groupWidth: "95%"},
+    legend: { position: "none" },
+  };
+  var chart = new google.visualization.ColumnChart(document.getElementById("chart5"));
   chart.draw(view, options);
 }
